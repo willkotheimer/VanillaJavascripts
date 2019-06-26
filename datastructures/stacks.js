0dbe8779d8b05a69d1201785da1baaa569ec1858
Will Kotheimer <wkotheimer@gmail.com>
2019-06-25 22:35:26 -0500

    Added stacks.js

--- datastructures/stacks.js
+++ datastructures/stacks.js
@@ -12,3 +12,19 @@ for(var i=0; i<word.length; i++) {
 letters.push(word[i]);
 
 }
+
+//pop off the stack in reverse order
+
+for(var i=0; i < word.length; i++) {
+rword += letters.pop();
+
+}
+
+if(rword === word) {
+console.log(word + " is a palindrome");
+
+} else {
+
+console.log(word + " is not a palindrome");
+
+}
