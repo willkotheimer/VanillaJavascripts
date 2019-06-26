7d92f9b5d23e6541de4ea75efd1d62995fa2d328
Will Kotheimer <wkotheimer@gmail.com>
2019-06-25 22:02:29 -0500

    added some folders, call,apply,bind

---
+++ callapplybind/callapplybind.js
@@ -0,0 +1,23 @@
+//call
+
+var obj = {num:2};
+var obj2 = {num:5};
+
+var addToThis =  function(a,b,c){
+	return this.num + a + b + c;
+}
+
+//1st argument in call has to be the object
+console.log(addToThis.call(obj,1,2,3)); //functionname.call(obj,functionarguments)
+
+//apply works with array arguments:
+
+var arr = [1,2,3];
+console.log(addToThis.apply(obj,arr)); 
+console.log(addToThis.apply(obj2,arr));
+
+//bind
+
+var bound = addToThis.bind(obj);
+
+console.log(bound(...arr));
---
+++ datastructures/stacks.js
@@ -0,0 +1,14 @@
+/*stacks*/
+
+var letters = [];
+
+var word = "racecar";
+
+var rword = "";
+
+//put letters of word into stack
+
+for(var i=0; i<word.length; i++) {
+letters.push(word[i]);
+
+}
